156325a53f4fd827ef36e4f1001b00503d850fe9
TmsLeOnGithub <tomasleon2012@gmail.com>
2022-06-04 13:18:26 -0300

    Desafio 4 clase 8 DOM

---
+++ js/produccionesTms.js
@@ -0,0 +1,114 @@
+//OBJETOS
+
+function Servicio (codigo,nombre,precio){
+    this.codigo=codigo;
+    this.nombre= nombre;
+    this.precio= precio;
+};
+
+let servicio1= new Servicio (1, "Fotografia", 800);
+let servicio2= new Servicio (2, "Video", 1200);
+let servicio3= new Servicio (3,"Drone",3000);
+
+let servicios = [servicio1,servicio2,servicio3]; //array
+
+let tablaServicios = document.getElementById ("tabla-servicios");
+
+console.log(tablaServicios)
+
+for (const servicio of servicios) {
+let fila = document.createElement("tr");
+fila.innerHTML= `<td> ${servicio.codigo}</td> 
+                <td> ${servicio.nombre} </td>
+                <td> $${servicio.precio}</td>`;
+  tablaServicios.append(fila);              
+    
+}
+
+
+// CALCULAR COSTO DE SERVICIOS SELECCIONADOS
+
+function calcularSubtotal (precioProducto, cantidadHoras){
+    return (precioProducto * cantidadHoras);
+ }
+
+ function getPrecioProducto(productoDeseado) {
+
+    const servicio = servicios.find(servicio => servicio.codigo === productoDeseado);
+
+    if(servicio){
+        return servicio.precio;
+    }else  {
+        alert("El codigo ingresado es inválido");
+        solicitarCodigoProducto();
+    }
+}
+
+//----------------------------------------------------------------------------------
+
+function calcularPresupuesto() {
+    let productoDeseado = parseInt(prompt("CODIGO 1 - FOTOS, CODIGO 2 - VIDEO, CODIGO 3 - DRONE"));
+    let precioProducto = getPrecioProducto(productoDeseado);
+    let cantidadHoras = parseInt(prompt("Ingrese cantidad de horas para el servicio elegido"));
+    let subtotal = calcularSubtotal(precioProducto, cantidadHoras);
+    return subtotal; 
+}
+
+
+    let total = 0;
+
+let finalizado;
+
+while(finalizado != "NO") {
+    alert("A continuación ingrese el código del producto deseado");
+    let subtotal = calcularPresupuesto();
+    total = total + subtotal;
+
+    finalizado = prompt("¿Desea sumar más servicios?").toUpperCase();
+}
+
+let divResumenPresupuesto= document.getElementById("resumenPresupuesto");
+let h6TotalSinDescuento = document.createElement("h6");
+h6TotalSinDescuento.innerText= `Total: $${total}`;
+divResumenPresupuesto.append(h6TotalSinDescuento);
+
+//cupon descuento
+
+    let cuponDescuento= "tt"
+    let cuponIngresado=prompt("INGRESE CUPON DE DESCUENTO    (tt)");
+    if(cuponIngresado == cuponDescuento){
+        let descuento = 300;
+        total = total- descuento    
+        alert("se aplico el cupon de descuento")
+
+        let h6TotalConDescuento = document.createElement("h6");
+        h6TotalConDescuento.innerText= `Total con descuento: $${total}`;
+        divResumenPresupuesto.append(h6TotalConDescuento);
+    }
+
+
+//CUOTAS
+
+let cuotas = parseInt(prompt("¿En cuantas cuotas queres pagar: 1,3,6,o 12?"))
+
+while(cuotas !== 1 && cuotas !== 3 && cuotas !== 6 && cuotas !== 12){
+    alert ("Solo tenemos 1,3,6 y 12 cuotas")
+    cuotas = parseInt(prompt("¿En cuantas cuotas queres pagar: 1,3,6,o 12?"))
+}
+
+let precioCuotas = Math.round(total / cuotas);
+
+let h6Cuotas = document.createElement("h6");
+h6Cuotas.innerHTML= `<strong>Serian ${cuotas} cuotas de $${precioCuotas}</strong>`;
+divResumenPresupuesto.append(h6Cuotas);
+
+
+// crear elementos Td q contengan lo que se guarda en los prompts
+// con eventos de mouse habilitar/desabhilitar opciones 
+
+
+
+
+
+
+

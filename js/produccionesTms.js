c8632556fda15c47988d16c7f956cc857738a7c2
TmsLeOnGithub <tomasleon2012@gmail.com>
2022-06-16 16:50:04 -0300

    Segunda entrega proyecto final

--- js/produccionesTms.js
+++ js/produccionesTms.js
@@ -1,82 +1,95 @@
 //OBJETOS
 
-function Servicio (codigo,nombre,precio){
+function Servicio (codigo,nombre,precio,horas,subtotal){
     this.codigo=codigo;
     this.nombre= nombre;
     this.precio= precio;
+    this.horas= horas;
+    this.subtotal=subtotal;
 };
 
-let servicio1= new Servicio (1, "Fotografia", 800);
-let servicio2= new Servicio (2, "Video", 1200);
-let servicio3= new Servicio (3,"Drone",3000);
+function Presupuesto (serviciosElegidos, total){
+    this.serviciosElegidos=serviciosElegidos
+    this.total=total
+}
 
-let servicios = [servicio1,servicio2,servicio3]; //array
+let servicio1= new Servicio (1, "Fotografia", 800, null,0);
+let servicio2= new Servicio (2, "Video", 1200,null,0);
+let servicio3= new Servicio (3,"Drone",3000,null,0);
+
+let serviciosDisponibles = [servicio1,servicio2,servicio3]; //array
+
+let servicios = JSON.parse(localStorage.getItem("servicios"));
+
+if(servicios === null){
+    localStorage.setItem("servicios", JSON.stringify (serviciosDisponibles));
+    servicios = JSON.parse(localStorage.getItem("servicios"));
+}
+
+
+
 
 let tablaServicios = document.getElementById ("tabla-servicios");
-
-console.log(tablaServicios)
 
 for (const servicio of servicios) {
 let fila = document.createElement("tr");
 fila.innerHTML= `<td> ${servicio.codigo}</td> 
                 <td> ${servicio.nombre} </td>
-                <td> $${servicio.precio}</td>`;
+                <td> $${servicio.precio}</td>
+                <td> <input id="${servicio.codigo}" type="number" value="${servicio.horas}"/></td>
+                <td id="subtotal-${servicio.codigo}"> ${servicio.subtotal != null ? servicio.subtotal : '' }</td>`;
+                
   tablaServicios.append(fila);              
     
 }
 
+servicios.forEach(servicio =>
+    {
+        document.getElementById(servicio.codigo).addEventListener('change', function(event) {
+            servicio.horas = parseInt(event.target.value);
+            calcularSubtotal(servicio)
+        })
+    })
+
 
 // CALCULAR COSTO DE SERVICIOS SELECCIONADOS
 
-function calcularSubtotal (precioProducto, cantidadHoras){
-    return (precioProducto * cantidadHoras);
+function calcularSubtotal (servicio){
+    servicio.subtotal = (servicio.precio * servicio.horas);
+    const tdSubtotal =  document.getElementById(`subtotal-${servicio.codigo}`);
+    tdSubtotal.innerHTML = `<strong>${servicio.subtotal}</strong>`;
+    calcularTotal();
  }
 
  function getPrecioProducto(productoDeseado) {
-
     const servicio = servicios.find(servicio => servicio.codigo === productoDeseado);
 
     if(servicio){
         return servicio.precio;
     }else  {
         alert("El codigo ingresado es inválido");
-        solicitarCodigoProducto();
+        //solicitarCodigoProducto();
     }
 }
 
 //----------------------------------------------------------------------------------
 
-function calcularPresupuesto() {
-    let productoDeseado = parseInt(prompt("CODIGO 1 - FOTOS, CODIGO 2 - VIDEO, CODIGO 3 - DRONE"));
-    let precioProducto = getPrecioProducto(productoDeseado);
-    let cantidadHoras = parseInt(prompt("Ingrese cantidad de horas para el servicio elegido"));
-    let subtotal = calcularSubtotal(precioProducto, cantidadHoras);
-    return subtotal; 
+let total = 0;
+
+function calcularTotal(){
+    total = 0;
+
+    servicios.forEach(servicio => {
+        total += servicio.subtotal;
+    })
+
+    let h6Total= document.getElementById("total");
+    h6Total.innerText= `Total: $${total}`;
 }
-
-
-    let total = 0;
-
-let finalizado;
-
-while(finalizado != "NO") {
-    alert("A continuación ingrese el código del producto deseado");
-    let subtotal = calcularPresupuesto();
-    total = total + subtotal;
-
-    finalizado = prompt("¿Desea sumar más servicios?").toUpperCase();
-}
-
-let divResumenPresupuesto= document.getElementById("resumenPresupuesto");
-let h6TotalSinDescuento = document.createElement("h6");
-h6TotalSinDescuento.innerText= `Total: $${total}`;
-divResumenPresupuesto.append(h6TotalSinDescuento);
 
 //cupon descuento
 
-    let cuponDescuento= "tt"
-
-
+let cuponDescuento= "tt"
 
 function aplicarCupon() {
     const inputCupon = document.getElementById("cupon");
@@ -109,9 +122,10 @@ function aplicarCuotas(){
 
 
 
-
-
-
+function guardarPresupuesto(){
+    const presupuesto= new Presupuesto(servicios,total)
+    localStorage.setItem (`presupuesto`, JSON.stringify(presupuesto))
+}
 
 
 
